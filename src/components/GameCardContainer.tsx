4acd226543608d4d7f077df8a39c1c024a402f55
myaomyaofish <qf.zheng@outlook.com>
2023-05-15 16:46:50 +0800

    Refactor: remove duplicated styles

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box width="260px" borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
