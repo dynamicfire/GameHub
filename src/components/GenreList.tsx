2025df02ce777a4987582f39a266984ccd164352
myaomyaofish <qf.zheng@outlook.com>
2023-05-25 15:40:04 +0800

    Clean up genrelist

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,11 @@
-import { Button, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { Button, HStack, Heading, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreListSkeleton from "./GenreListSkeleton";
 
 interface Props {
-    onSelectGenre: (genre: Genre) => void;
-    selectedGenre: Genre | null;
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
@@ -13,6 +13,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
   return (
     <>
+      <Heading fontSize={'2xl'} marginBottom={'5px'}>Genres</Heading>
       {error && null}
       {isLoading && <GenreListSkeleton />}
       <List>
@@ -22,12 +23,17 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
               <Image
                 boxSize="32px"
                 borderRadius={8}
+                objectFit={'cover'}
                 src={getCroppedImageUrl(genre.image_background, 600, 400)}
               ></Image>
               <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
                 fontSize={"lg"}
                 variant={"link"}
-                onClick={() => {onSelectGenre(genre)}}
+                onClick={() => {
+                  onSelectGenre(genre);
+                }}
                 fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
               >
                 {genre.name}
